8e4ce30da0ca7dc3da0ed8da8103b9b1f4ad2ec2
Irving <Irvingb232@gmail.com>
2016-01-14 17:11:44 -0800

    [Refactor] Fixes link to fb

--- client/components/contact.js
+++ client/components/contact.js
@@ -10,7 +10,7 @@ class Contact extends React.Component {
         <span className='vertical-line'></span>
         <a className='email' href='mailto:irvingb232@gmail.com'>irvingb232@gmail.com</a>
         <div className='icons'>
-          <a className='icon-container' href='https://www.facebook.com/irvingaxelb'><img className='icon' src='../styles/public/assets/facebook.png' /></a>
+          <a className='icon-container' href='https://www.facebook.com/irvingabb'><img className='icon' src='../styles/public/assets/facebook.png' /></a>
           <a className='icon-container' href='https://twitter.com/irvingaxelb'><img className='icon' src='../styles/public/assets/twitter.png' /></a>
           <a className='icon-container' href='https://github.com/IrvingAxelB'><img className='icon' src='../styles/public/assets/git.png' /></a>
           <a className='icon-container' href='https://www.linkedin.com/in/irvingbarajas'><img className='icon' src='../styles/public/assets/linkedin.png' /></a>
