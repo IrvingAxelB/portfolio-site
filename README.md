4b8a1c222cda7e56b63cccef56c12d091e486a2b
Irving <Irvingb232@gmail.com>
2016-01-14 16:59:41 -0800

    [Adds] Readme instructions

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # portfolio-site
+
+Welcome to my website! I built this using React, React-Router, Babel, and Webpack.
+
+- Run   npm install
+- npm start
--- client/components/about.js
+++ client/components/about.js
@@ -13,42 +13,42 @@ class About extends React.Component {
         <div className='specialized-table'>
           <div className='skills-container'>
             <img className='skills-img' src='' />
-            <h4 className='skills-header'></h4>
+            <h4 className='skills-header'>Programming</h4>
             <span className='skills-line'></span>
             <p className='skills-description'></p>
           </div>
 
           <div className='skills-container'>
             <img className='skills-img' src='' />
-            <h4 className='skills-header'></h4>
+            <h4 className='skills-header'>Interaction Design</h4>
             <span className='skills-line'></span>
-            <p className='skills-description'></p>
+            <p className='skills-description'>A key area of focus in my design process is to ensure the best interaction for each situation to guide the user in their digital journey, as this is essential in creating engaging and effective experiences.</p>
           </div>
 
           <div className='skills-container'>
             <img className='skills-img' src='' />
-            <h4 className='skills-header'></h4>
+            <h4 className='skills-header'>Concepting</h4>
             <span className='skills-line'></span>
-            <p className='skills-description'></p>
+            <p className='skills-description'>My interests in many different fields has helped me to stay curious, open and flexible. These attributes have been essential in the concepting of digital ideas.</p>
           </div>
 
           <div className='skills-container'>
             <img className='skills-img' src='' />
-            <h4 className='skills-header'></h4>
+            <h4 className='skills-header'>Prototyping</h4>
             <span className='skills-line'></span>
             <p className='skills-description'></p>
           </div>
          
           <div className='skills-container'>
             <img className='skills-img' src='' />
-            <h4 className='skills-header'></h4>
+            <h4 className='skills-header'>Learning</h4>
             <span className='skills-line'></span>
             <p className='skills-description'></p>
           </div>
          
           <div className='skills-container'>
             <img className='skills-img' src='' />
-            <h4 className='skills-header'></h4>
+            <h4 className='skills-header'>Teaching</h4>
             <span className='skills-line'></span>
             <p className='skills-description'></p>
           </div>
